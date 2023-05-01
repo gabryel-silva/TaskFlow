32902b8ec6a5feb56d9fdfd4c6d352113b2ef37f
Gabryel Silva <gabryelssantos@gmail.com>
2023-05-01 16:32:47 -0300

    v1.0.0

---
+++ assets/js/instrucoes.js
@@ -0,0 +1,8 @@
+/*const botao = document.getElementById('botao')
+const dropDown = document.querySelector('.container-fechado')
+const arrowDirection  = document.querySelector('.arrow-direction-right')
+
+botao.addEventListener('click', () => {
+    dropDown.classList.toggle('container-active')
+    arrowDirection.classList.toggle('arrow-direction-bottom')
+})*/
---
+++ assets/js/linksInternosTransicao.js
@@ -0,0 +1,13 @@
+/*const linkInicio = document.getElementById('inicio')
+const linkSobreFerramenta = document.getElementById('sobreFerramenta')
+const linkInstrucoes = document.getElementById('instrucoes')
+const linkDesativado = document.querySelector('.menu-inicio')
+const itemMenu = document.querySelector('.item-menu')
+
+function VerificaLinkAtivo(){
+    itemMenu.addEventListener('click', () => {
+        linkDesativado.classList.toggle('active')
+    })
+}
+
+VerificaLinkAtivo()*/
